b8875e9eaa08d0343b0f00c059c602f7d51168ea
Tyler Long <tyler4long@gmail.com>
2023-04-12 12:19:03 -0700

    Add sample output

--- RingCentral.PubNub.Demo/Program.cs
+++ RingCentral.PubNub.Demo/Program.cs
@@ -26,3 +26,10 @@ var subscription = await pubNubExtension.Subscribe(eventFilters, message =>
 // Wait for 60 seconds before the app exits
 // In the mean time, send SMS to trigger a notification for testing purpose
 await Task.Delay(60000);
+
+/*
+ * Sample output:
+ I got a notification:
+{"uuid":"2848583747434852364","event":"/restapi/v1.0/account/37439510/extension/850957020/message-store","timestamp":"2023-04-12T19:17:36.821Z","subscriptionId":"3ce48c91-e4c0-4123-86be-0e36c4e1c6ba","ownerId":"850957020","body":{"accountId":37439510,"extensionId":850957020,"lastUpdated":"2023-04-12T19:17:24.693Z","changes":[{"type":"SMS","newCount":1,"updatedCount":0,"newMessageIds":[1729816321020]}]}}
+ */
+ 
